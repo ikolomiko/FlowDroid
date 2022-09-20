cf0cefd99ee704433c9b3735203fd230c6c1a8f0
İlker Avcı <ikolomiko@gmail.com>
2022-09-20 23:38:33 +0300

    Add android.jar and change lots of stuff in the runner script

--- flowdroid-libsec-wrapper.sh
+++ flowdroid-libsec-wrapper.sh
@@ -5,21 +5,25 @@ SECONDS=0
 JAVA_BIN="/usr/bin/java" # Java version >= 1.8
 JAVAC_BIN="/usr/bin/javac"
 KOTLINC_BIN="/usr/bin/kotlinc"
-ANDROID_JAR="$HOME/Android/platforms/android-29/android.jar"
 
 FLOWDROID_ROOT="$(dirname $(realpath "$0"))"
 FLOWDROID_BIN="$FLOWDROID_ROOT/soot-infoflow-cmd/target/soot-infoflow-cmd-jar-with-dependencies.jar"
 SOURCES_SINKS="$FLOWDROID_ROOT/SourcesAndSinks.txt"
-FLOWDROID_OPT=" -ls -r -os -mc 99999 -md 99999 -mt 8 -ps"
+ANDROID_JAR="$FLOWDROID_ROOT/android-29.jar"
+FLOWDROID_OPT=" -r -os -mc 99999 -md 99999 -mt 8 -ps"
 
 IVY_BIN="$FLOWDROID_ROOT/ivy-2.5.0.jar"
 IVY_SETTINGS="$FLOWDROID_ROOT/ivysettings.xml"
 
-INPUT_FILE="$1" # GROUPID+ARTIFACTID+VERSION.aar || GROUPID+ARTIFACTID+VERSION.jar
-OUTPUT_XML="$2"
+LIBRARY_ID="$1" # GROUP_ID+ARTIFACT_ID+VERSION
+INPUT_FILE="$2" # <file>.aar || <file>.jar
+OUTPUT_PATH="$3" # <output directory path>
+
+RED='\033[0;31m'
+NC='\033[0m'
 
 usage() {
-  echo "Usage: ./analyze.sh <jar or aar file> <output xml file>"
+  echo "Usage: ./analyze.sh <GROUPID+ARTIFACTID+VERSION> <jar or aar file> <output directory path>"
   exit 1
 }
 
@@ -33,13 +37,13 @@ run_flowdroid() {
   # $1 = path to the unzipped jar file
   # $2 = group id
   # $3 = classpath
-  $JAVA_BIN -jar $FLOWDROID_BIN -a $1 -s $SOURCES_SINKS -o $OUTPUT_XML -p $ANDROID_JAR -gi $2 -ac $3 $FLOWDROID_OPT
+  $JAVA_BIN -jar $FLOWDROID_BIN -a $1 -s $SOURCES_SINKS -o $OUTPUT_PATH/flowdroid-results.xml -p $ANDROID_JAR -gi $2 -ac $3 $FLOWDROID_OPT
 }
 
 
 # Check arguments
-if [ -z $OUTPUT_XML ]; then
-  # Either the second or both arguments are empty
+if [ -z $OUTPUT_PATH ]; then
+  # At least one of the arguments is empty
   usage
 fi
 
@@ -47,11 +51,9 @@ if [ ! -f $INPUT_FILE ]; then
   error "File $INPUT_FILE does not exist"
 fi
 
-OUTPUT_XML=$(realpath $OUTPUT_XML)
 
-if [ -f $OUTPUT_XML ]; then
-  rm $OUTPUT_XML
-fi
+OUTPUT_PATH=$(realpath $(dirname $OUTPUT_PATH))
+mkdir -p $OUTPUT_PATH
 
 
 # Check input file extension
@@ -63,12 +65,11 @@ fi
 
 # Check input file name
 BASE_NAME=$(basename $INPUT_FILE)
-IFS=+ read -r GROUPID ARTIFACTID VERSION <<< $BASE_NAME
-VERSION=${VERSION%.*}
+IFS=+ read -r GROUPID ARTIFACTID VERSION <<< $LIBRARY_ID
 if [[ -z $GROUPID || -z $ARTIFACTID || -z $VERSION ]]; then
-  error "Incorrect file name format: Expected <group id>+<artifact id>+<version>.<aar|jar> but got $BASE_NAME
+  error "Incorrect input format: Expected <group id>+<artifact id>+<version> file.<aar|jar> but got $LIBRARY_ID $INPUT_FILE
 
-Example input file name format: com.android.google.material+material+1.6.1.aar"
+Example input file name format: com.android.google.material+material+1.6.1 path/to/1.6.1.aar"
 fi
 
 # Extract aar/jar file
@@ -86,10 +87,12 @@ fi
 
 
 # Get all dependencies (except android.jar)
-DEPS_DIR=$(mktemp -d)
-$JAVA_BIN -jar $IVY_BIN -dependency $GROUPID $ARTIFACTID $VERSION -retrieve "$DEPS_DIR/[organization]+[artifact]+[revision](+[classifier]).[ext]" -settings $IVY_SETTINGS
-rm -f $DEPS_DIR/$BASE_NAME
-
+get_dependencies() {
+  DEPS_DIR=$(mktemp -d)
+  $JAVA_BIN -jar $IVY_BIN -dependency $GROUPID $ARTIFACTID $VERSION -retrieve "$DEPS_DIR/[organization]+[artifact]+[revision](+[classifier]).[ext]" -settings $IVY_SETTINGS
+  rm -f $DEPS_DIR/$BASE_NAME
+}
+get_dependencies 2>&1 | tee $OUTPUT_PATH/ivy-log.txt 
 
 # Unpack aar dependencies (if there are any)
 AAR_DEPS=$(find $DEPS_DIR -name "*.aar")
@@ -115,16 +118,22 @@ KOTLIN_FILES=$(find $TEMP -name '*.kt')
 
 # Compile source files to bytecode
 cd $TEMP
-[[ ! -z $JAVA_FILES ]] && javac -proc:none -cp $CLASSPATH $JAVA_FILES
-[[ ! -z $KOTLIN_FILES ]] && kotlinc -classpath $CLASSPATH $KOTLIN_FILES
+if [[ ! -z $JAVA_FILES ]]; then 
+  echo $RED Compiling java source files for $LIBRARY_ID+$VERSION $NC
+  javac -proc:none -cp $CLASSPATH $JAVA_FILES
+fi
+if [[ ! -z $KOTLIN_FILES ]]; then 
+  echo $RED Compiling kotlin source files for $LIBRARY_ID+$VERSION $NC
+  kotlinc -classpath $CLASSPATH $KOTLIN_FILES
+fi
 
 
 # Run FlowDroid (what a useful comment)
-run_flowdroid $TEMP $GROUPID $CLASSPATH
+run_flowdroid $TEMP $GROUPID $CLASSPATH 2>&1 | tee $OUTPUT_PATH/flowdroid-log.txt
 
 
 # Clean up the temporary directory and exit
-rm -r $TEMP
-rm -r $DEPS_DIR
+rm -rf $TEMP
+rm -rf $DEPS_DIR
 echo "Took $SECONDS seconds"
 exit 0
