fcbe27b16f7d18b509dd97ec264fd107e625b966
İlker Avcı <ikolomiko@gmail.com>
2022-12-20 22:47:57 +0300

    wrapper: add rmode and a bug about aar deps

--- flowdroid-libsec-wrapper.sh
+++ flowdroid-libsec-wrapper.sh
@@ -13,15 +13,17 @@ SOURCES_SINKS="$FLOWDROID_ROOT/SourcesAndSinks.txt"
 ANDROID_JAR="$FLOWDROID_ROOT/android-29.jar"
 FLOWDROID_OPT=" -r -os -mc 99999 -md 99999 -mt 8 -ps"
 POMS_DIR="$FLOWDROID_ROOT/../libsec-scraper/poms"
+DEPS_ROOT="$FLOWDROID_ROOT/deps"
 
 IVY_BIN="$FLOWDROID_ROOT/ivy-2.5.0.jar"
 IVY_SETTINGS="$FLOWDROID_ROOT/ivysettings.xml"
 MVN_BIN="/usr/bin/mvn"
 MVN_SETTINGS="$FLOWDROID_ROOT/mavensettings.xml"
 
-LIBRARY_ID="$1" # GROUP_ID+ARTIFACT_ID+VERSION
-INPUT_FILE="$2" # <file>.aar || <file>.jar
+LIBRARY_ID="$1"  # GROUP_ID+ARTIFACT_ID+VERSION
+INPUT_FILE="$2"  # <file>.aar || <file>.jar
 OUTPUT_PATH="$3" # <output directory path>
+RMODE="$4"       # [all,deps,flowdroid] default=all
 
 RED='\033[0;31m'
 NC='\033[0m'
@@ -29,51 +31,47 @@ NC='\033[0m'
 PAC_RESOLVER="ivy" # or mvn
 
 usage() {
-  echo "Usage: ./analyze.sh <GROUPID+ARTIFACTID+VERSION> <jar or aar file> <output directory path>"
-  exit 1
+    echo "Usage: ./analyze.sh <GROUPID+ARTIFACTID+VERSION> <jar or aar file> <output directory path>"
+    exit 1
 }
 
 error() {
-  echo "An error occurred: 
+    echo "An error occurred: 
 $1"
-  exit 1
+    exit 1
 }
 
-run_flowdroid() { 
-  # $1 = path to the unzipped jar file
-  # $2 = group id
-  # $3 = classpath
-  $JAVA_BIN -jar $FLOWDROID_BIN -a $1 -s $SOURCES_SINKS -o $OUTPUT_PATH/flowdroid-results.xml -p $ANDROID_JAR -gi $2 -ac $3 $FLOWDROID_OPT
+run_flowdroid() {
+    # $1 = path to the unzipped jar file
+    # $2 = group id
+    # $3 = classpath
+    $JAVA_BIN -jar $FLOWDROID_BIN -a $1 -s $SOURCES_SINKS -o $OUTPUT_PATH/flowdroid-results.xml -p $ANDROID_JAR -gi $2 -ac $3 $FLOWDROID_OPT
 }
-
 
 # Check arguments
 if [ -z $OUTPUT_PATH ]; then
-  # At least one of the arguments is empty
-  usage
+    # At least one of the arguments is empty
+    usage
 fi
 
 if [ ! -f $INPUT_FILE ]; then
-  error "File $INPUT_FILE does not exist"
+    error "File $INPUT_FILE does not exist"
 fi
-
 
 mkdir -p $OUTPUT_PATH
 OUTPUT_PATH=$(realpath $OUTPUT_PATH)
 
-
 # Check input file extension
 EXT=$(echo ${INPUT_FILE##*.} | tr '[:upper:]' '[:lower:]')
 if [[ ! $EXT =~ ^(aar|jar)$ ]]; then
-  error "Incorrect file extension: Expected aar or jar but got $EXT"
+    error "Incorrect file extension: Expected aar or jar but got $EXT"
 fi
-
 
 # Check input file name
 BASE_NAME=$(basename $INPUT_FILE)
-IFS=+ read -r GROUPID ARTIFACTID VERSION <<< $LIBRARY_ID
+IFS=+ read -r GROUPID ARTIFACTID VERSION <<<$LIBRARY_ID
 if [[ -z $GROUPID || -z $ARTIFACTID || -z $VERSION ]]; then
-  error "Incorrect input format: Expected <group id>+<artifact id>+<version> file.<aar|jar> but got $LIBRARY_ID $INPUT_FILE
+    error "Incorrect input format: Expected <group id>+<artifact id>+<version> file.<aar|jar> but got $LIBRARY_ID $INPUT_FILE
 
 Example input file name format: com.android.google.material+material+1.6.1 path/to/1.6.1.aar"
 fi
@@ -81,89 +79,94 @@ fi
 # Extract aar/jar file
 TEMP=$(mktemp -d)
 if [ $EXT = "aar" ]; then
-  TEMP2=$(mktemp -d)
-  unzip $INPUT_FILE classes.jar -d $TEMP2
-  unzip $TEMP2/classes.jar -d $TEMP
-  rm -rf $TEMP2
+    TEMP2=$(mktemp -d)
+    unzip $INPUT_FILE classes.jar -d $TEMP2
+    unzip $TEMP2/classes.jar -d $TEMP
+    rm -rf $TEMP2
 else
-  unzip $INPUT_FILE -d $TEMP
+    unzip $INPUT_FILE -d $TEMP
 fi
 
-DEPS_DIR=$(mktemp -d)
-pomfile=""
-# Create pom file
-create_pom() {
-  original_pom="$POMS_DIR/$GROUPID/$ARTIFACTID/$VERSION/pom.xml"
-  if [[ ! -f $original_pom ]]; then
-    return 0
-  fi
+# Dependency resolving
+DEPS_DIR="$DEPS_ROOT/$GROUPID+$ARTIFACTID/$VERSION"
+mkdir -p $DEPS_DIR
+if [[ -z $RMODE || $RMODE == "all" || $RMODE == "deps" ]]; then
+    pomfile=""
+    # Create pom file if PAC_RESOLVER is set to "mvn"
+    create_pom() {
+        original_pom="$POMS_DIR/$GROUPID/$ARTIFACTID/$VERSION/pom.xml"
+        if [[ ! -f $original_pom ]]; then
+            return 0
+        fi
 
-  mkdir -p $DEPS_DIR/pom
-  pomfile=$DEPS_DIR/pom/pom.xml
-  cp $original_pom $pomfile
-  python3 $FLOWDROID_ROOT/inject_plugin.py "$pomfile"
+        mkdir -p $DEPS_DIR/pom
+        pomfile=$DEPS_DIR/pom/pom.xml
+        cp $original_pom $pomfile
+        python3 $FLOWDROID_ROOT/inject_plugin.py "$pomfile"
 
-  echo $(realpath $pomfile)
-}
-[[ $PAC_RESOLVER == "mvn" ]] && pomfile="$(create_pom)"
+        echo $(realpath $pomfile)
+    }
+    [[ $PAC_RESOLVER == "mvn" ]] && pomfile="$(create_pom)"
 
-# Get all dependencies (except android.jar)
-if [[ $PAC_RESOLVER == "ivy" ]]; then
-  get_dependencies() {
-    $JAVA_BIN -jar $IVY_BIN -dependency $GROUPID $ARTIFACTID $VERSION -retrieve "$DEPS_DIR/[organization]+[artifact]+[revision](+[classifier]).[ext]" -settings $IVY_SETTINGS -cache cachedir
-    rm -f $DEPS_DIR/$BASE_NAME
-  }
-  get_dependencies 2>&1 | tee $OUTPUT_PATH/ivy-log.txt
-elif [[ $PAC_RESOLVER == "mvn" ]]; then
-  get_dependencies() {
-    JAVA_HOME="" mvn dependency:copy-dependencies -f $pomfile -DoutputDirectory=$DEPS_DIR -s $MVN_SETTINGS -gs $MVN_SETTINGS -fae
-  }
-  [ ! -z $pomfile ] && get_dependencies 2>&1 | tee $OUTPUT_PATH/mvn-log.txt || echo "pom.xml was not found"
+    # Get all dependencies (except android.jar)
+    if [[ $PAC_RESOLVER == "ivy" ]]; then
+        get_dependencies() {
+            $JAVA_BIN -jar $IVY_BIN -dependency $GROUPID $ARTIFACTID $VERSION -retrieve "$DEPS_DIR/[organization]+[artifact]+[revision](+[classifier]).[ext]" -settings $IVY_SETTINGS -cache cachedir
+            rm -f $DEPS_DIR/$BASE_NAME
+        }
+        get_dependencies 2>&1 | tee $OUTPUT_PATH/ivy-log.txt
+    elif [[ $PAC_RESOLVER == "mvn" ]]; then
+        get_dependencies() {
+            JAVA_HOME="" mvn dependency:copy-dependencies -f $pomfile -DoutputDirectory=$DEPS_DIR -s $MVN_SETTINGS -gs $MVN_SETTINGS -fae
+        }
+        [ ! -z $pomfile ] && get_dependencies 2>&1 | tee $OUTPUT_PATH/mvn-log.txt || echo "pom.xml was not found"
+    fi
+
+    [[ $RMODE == "deps" ]] && exit 0
 fi
 
 # Unpack aar dependencies (if there are any)
 AAR_DEPS=$(find $DEPS_DIR -name "*.aar")
 while IFS= read -r line; do
-  [ -z $line ] && break
-  unzip $line classes.jar -d "$DEPS_DIR/$(uuidgen).jar"
-  rm -f $line
-done <<< "$AAR_DEPS"
-
+    [ -z $line ] && break
+    tempout=$(mktemp -d)
+    bname=$(basename $1)
+    filename="${bname%.*}"
+    unzip $line classes.jar -d $tempout
+    mv $tempout/classes.jar $DEPS_DIR/$filename.jar
+    rm -f $line
+    rm -rf tempout
+done <<<"$AAR_DEPS"
 
 # Add dependencies to the classpath
 CLASSPATH="$ANDROID_JAR:$TEMP"
-for lib in $DEPS_DIR/*
-do
-  [ -f $lib ] && CLASSPATH="$CLASSPATH:$lib"
+for lib in $DEPS_DIR/*; do
+    [ -f $lib ] && CLASSPATH="$CLASSPATH:$lib"
 done
-
 
 # Find java and kotlin source files
 JAVA_FILES=$(find $TEMP -name '*.java')
 KOTLIN_FILES=$(find $TEMP -name '*.kt')
 
-
 # Compile source files to bytecode (if there are any)
 compile() {
-  cd $TEMP
-  if [[ ! -z $JAVA_FILES ]]; then 
-    echo $RED Compiling java source files for $LIBRARY_ID+$VERSION $NC
-    javac -proc:none -cp $CLASSPATH $JAVA_FILES
-  fi
-  if [[ ! -z $KOTLIN_FILES ]]; then 
-    echo $RED Compiling kotlin source files for $LIBRARY_ID+$VERSION $NC
-    kotlinc -classpath $CLASSPATH $KOTLIN_FILES
-  fi
+    cd $TEMP
+    if [[ ! -z $JAVA_FILES ]]; then
+        echo $RED Compiling java source files for $LIBRARY_ID+$VERSION $NC
+        javac -proc:none -cp $CLASSPATH $JAVA_FILES
+    fi
+    if [[ ! -z $KOTLIN_FILES ]]; then
+        echo $RED Compiling kotlin source files for $LIBRARY_ID+$VERSION $NC
+        kotlinc -classpath $CLASSPATH $KOTLIN_FILES
+    fi
 }
 [[ ! -z $JAVA_FILES && ! -z $KOTLIN_FILES ]] && compile 2>&1 | tee $OUTPUT_PATH/compile-log.txt
 
 # Run FlowDroid (what a useful comment)
 run_flowdroid $TEMP $GROUPID $CLASSPATH 2>&1 | tee $OUTPUT_PATH/flowdroid-log.txt
 
-
 # Clean up the temporary directory and exit
 rm -rf $TEMP
-rm -rf $DEPS_DIR
 rm -rf $FLOWDROID_ROOT/cachedir
 echo "Took $SECONDS seconds"
 exit 0
